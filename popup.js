0e433eddfaba99f44dd8b65c7b26572ae9c99c45
Andre Oliveira Serra <a39946@alunos.ipb.pt>
2022-01-30 19:47:44 +0000

    Fixed issues with generating number, also fixed some issues with layout

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 
 function generateRandomnumber() {
-    var minNumber = document.getElementById("minNumber").value;
-    var maxNumber = document.getElementById("maxNumber").value;
-    var randomNumber = Math.floor((Math.random() * maxNumber) + minNumber);
+    var minNumber = parseInt(document.getElementById("minNumber").value);
+    var maxNumber = parseInt(document.getElementById("maxNumber").value);
+    var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
     document.getElementById("resultRandom").innerHTML = randomNumber;
 }
+
+document.getElementById("generateBtn").addEventListener("click", function () { generateRandomnumber(); });
